decc7c873ad21fd98ee0f21315c86f3c33f13f94
Codacy Archive <codacy-archive@local>
2025-12-22 11:05:41 +0000

    snapshot 2025-12-22T11:05:41.655077+00:00

--- src/test/java/org/apache/commons/lang3/math/NumberUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/math/NumberUtilsTest.java
@@ -1013,7 +1013,9 @@ class NumberUtilsTest extends AbstractLangTest {
         assertFalse(NumberUtils.isParsable("pendro"));
         assertFalse(NumberUtils.isParsable("64, 2"));
         assertFalse(NumberUtils.isParsable("64.2.2"));
-        assertFalse(NumberUtils.isParsable("64."));
+        assertFalse(NumberUtils.isParsable("64.."));
+        assertTrue(NumberUtils.isParsable("64."));
+        assertTrue(NumberUtils.isParsable("-64."));
         assertFalse(NumberUtils.isParsable("64L"));
         assertFalse(NumberUtils.isParsable("-"));
         assertFalse(NumberUtils.isParsable("--2"));
@@ -1025,6 +1027,9 @@ class NumberUtilsTest extends AbstractLangTest {
         assertTrue(NumberUtils.isParsable("-018"));
         assertTrue(NumberUtils.isParsable("-018.2"));
         assertTrue(NumberUtils.isParsable("-.236"));
+        assertTrue(NumberUtils.isParsable("2."));
+        // TODO assertTrue(NumberUtils.isParsable("2.f"));
+        // TODO assertTrue(NumberUtils.isParsable("2.d"));
     }
 
     /**
@@ -1077,6 +1082,11 @@ class NumberUtilsTest extends AbstractLangTest {
     @Test
     void testLang1729IsParsableDouble() {
         assertTrue(isParsableDouble("1"));
+        assertTrue(isParsableDouble("1."));
+        // TODO assertTrue(isParsableDouble("1.f"));
+        // TODO assertTrue(isParsableDouble("1.d"));
+        assertTrue(isParsableDouble("1.0"));
+        assertFalse(isParsableDouble("1.0."));
         assertFalse(isParsableDouble("1 2 3"));
         assertFalse(isParsableDouble("１ ２ ３"));
     }
@@ -1084,6 +1094,11 @@ class NumberUtilsTest extends AbstractLangTest {
     @Test
     void testLang1729IsParsableFloat() {
         assertTrue(isParsableFloat("1"));
+        assertTrue(isParsableFloat("1."));
+        // TODO assertTrue(isParsableFloat("1.f"));
+        // TODO assertTrue(isParsableFloat("1.d"));
+        assertTrue(isParsableFloat("1.0"));
+        assertFalse(isParsableFloat("1.0."));
         assertFalse(isParsableFloat("1 2 3"));
         assertFalse(isParsableFloat("１ ２ ３"));
     }
